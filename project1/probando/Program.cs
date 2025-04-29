7282a0286f4dfec628b531e0979d82989217b041
Victor Manuel Tapias Serna <tapiasv07@gmail.com>
2025-04-28 20:48:49 -0500

    SQL API key leak in Program.cs

--- project1/probando/Program.cs
+++ project1/probando/Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("Hello, Victor!");
 
 // Example of a fake API key for learning purposes
 string stripeApiKey = "sk_test_51H8bJqLw5gFkeXQtS3lPz8ZyVz1oRp3k8cZKHgXh9SKuXqlO1Z4Uif2KHIB2fhz3kqG8R1Kc7NOGaVZr6h1Pb0H900mD5MN6";
+
+// Example of a fake SQL Server connection string for learning purposes
+string testingConnection = "Server=tcp:fake-server.database.windows.net,1433;Initial Catalog=TestDB;Persist Security Info=False;User ID=fakeadmin;Password=FakePassword123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
