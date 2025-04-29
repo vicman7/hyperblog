360e2bb268141ae745ffa81c9fd7685942b865bc
Victor Manuel Tapias Serna <tapiasv07@gmail.com>
2025-04-28 20:39:17 -0500

    Update API secret key in Program.cs

--- project1/probando/Program.cs
+++ project1/probando/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Victor!");
 
-string stripeApiKey = "clave_sensible";
+// Example of a fake API key for learning purposes
+string stripeApiKey = "sk_test_51H8bJqLw5gFkeXQtS3lPz8ZyVz1oRp3k8cZKHgXh9SKuXqlO1Z4Uif2KHIB2fhz3kqG8R1Kc7NOGaVZr6h1Pb0H900mD5MN6";
